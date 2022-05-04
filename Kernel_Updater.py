eff288e71bdc65693ae72ef1fc715843689ba4e2
Immanuel Raj <immanuelr44@gmail.com>
2022-05-04 23:21:58 +0530

    Push new kernel source code

--- Kernel_Updater.py
+++ Kernel_Updater.py
@@ -3,9 +3,9 @@ import os
 
 # Variables - Changable
 KERNEL_VERSION = "4.19" # Set Kernel Version (4.4/4.14/4.19)
-KERNEL_TAG = "LA.UM.10.2.1.r1-03400-sdm660.0" # Set CAF Tag / Upstream Version (LA.UM.10.2.1.r1-0300-sdm660.0/v4.19.157)
+KERNEL_TAG = "LA.UM.10.2.1.r1-03600-sdm660.0" # Set CAF Tag / Upstream Version (LA.UM.10.2.1.r1-0300-sdm660.0/v4.19.157)
 REPO_LINK = "https://github.com/iamimmanuelraj/android_kernel_xiaomi_jasmine_sprout" # Repo link to pull/fetch/push Kernel
-BASE_BRANCH = "BASE" # Base branch to pick the old/device base changes from
+BASE_BRANCH = "BASE_10_3600" # Base branch to pick the old/device base changes from
 
 # Variables - Non_Changable
 QCACLD_LINK = "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0" # Qcacld repo link
@@ -40,7 +40,7 @@ os.system("git subtree add --prefix=fs/exfat %s master --squash"%(EXFAT_LINK))
 
 # Fetch the changes from old kernel and cherry pick em
 os.system("git fetch %s %s"%(REPO_LINK,BASE_BRANCH))
-os.system("git cherry-pick 57cc5c1209255fc5c6350a3cd3d36f8ee3730fd7^..0ca35dc53901d3ca4005d3d788ff6b9d714bd4d1")
+os.system("git cherry-pick 6113052fc218f0feb74258766d9ec102e9a2414a^..9ca97893c5e8d5cee6d6498309a8ae418bef60ce")
 
 # Change Localversion in defconfig (Add defconfig paths for your devices)
 DEFCONFIG = open("arch/arm64/configs/vendor/jasmine_sprout_defconfig", "r")
@@ -49,10 +49,22 @@ NUMBER_OF_LINES[0] = '''CONFIG_LOCALVERSION="-%s"\n'''%KERNEL_TAG
 DEFCONFIG = open("arch/arm64/configs/vendor/jasmine_sprout_defconfig", "w")
 DEFCONFIG.writelines(NUMBER_OF_LINES)
 DEFCONFIG.close()
+DEFCONFIG = open("arch/arm64/configs/vendor/jasmine_sprout-debug_defconfig", "r")
+NUMBER_OF_LINES = DEFCONFIG.readlines()
+NUMBER_OF_LINES[0] = '''CONFIG_LOCALVERSION="-%s"\n'''%KERNEL_TAG
+DEFCONFIG = open("arch/arm64/configs/vendor/jasmine_sprout-debug_defconfig", "w")
+DEFCONFIG.writelines(NUMBER_OF_LINES)
+DEFCONFIG.close()
 DEFCONFIG = open("arch/arm64/configs/vendor/wayne_defconfig", "r")
 NUMBER_OF_LINES = DEFCONFIG.readlines()
 NUMBER_OF_LINES[0] = '''CONFIG_LOCALVERSION="-%s"\n'''%KERNEL_TAG
 DEFCONFIG = open("arch/arm64/configs/vendor/wayne_defconfig", "w")
+DEFCONFIG.writelines(NUMBER_OF_LINES)
+DEFCONFIG.close()
+DEFCONFIG = open("arch/arm64/configs/vendor/wayne-debug_defconfig", "r")
+NUMBER_OF_LINES = DEFCONFIG.readlines()
+NUMBER_OF_LINES[0] = '''CONFIG_LOCALVERSION="-%s"\n'''%KERNEL_TAG
+DEFCONFIG = open("arch/arm64/configs/vendor/wayne-debug_defconfig", "w")
 DEFCONFIG.writelines(NUMBER_OF_LINES)
 DEFCONFIG.close()
 
